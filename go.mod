c9a69c31b0f15371cc69e5e80c44ab72127ab8e9
Sahitya Chandra <sahityajb@gmail.com>
2026-01-06 01:14:52 +0530

    completed mount fs using pivot root and added PTY and raw mode

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module github.com/sahitya-chandra/mini-docker
 
 go 1.24.5
 
-require golang.org/x/sys v0.39.0 // indirect
+require (
+	github.com/creack/pty v1.1.24
+	github.com/joho/godotenv v1.5.1
+	golang.org/x/sys v0.39.0
+	golang.org/x/term v0.29.0
+)
--- main.go
+++ main.go
@@ -2,15 +2,22 @@ package main
 
 import (
 	"fmt"
-	"golang.org/x/sys/unix"
+	"io"
 	"os"
 	"os/exec"
 	"os/signal"
+	"path/filepath"
 	"strings"
 	"syscall"
+
+	"github.com/creack/pty"
+	"github.com/joho/godotenv"
+	"golang.org/x/sys/unix"
+	"golang.org/x/term"
 )
 
 func main() {
+	godotenv.Load()
 	if len(os.Args) < 2 {
 		fmt.Println("Usage: run|child")
 		return
@@ -21,7 +28,7 @@ func main() {
 	} else if arg == "child" {
 		child()
 	} else {
-		fmt.Print("Unknown")
+		fmt.Printf("Unknown argument: %s\n", arg)
 	}
 }
 
@@ -33,156 +40,205 @@ func run() {
 
 	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
 
+	// Set up namespaces
+	cloneFlags := uintptr(syscall.CLONE_NEWPID | syscall.CLONE_NEWNS | syscall.CLONE_NEWUTS | syscall.CLONE_NEWIPC)
+
 	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Cloneflags: 
-			// syscall.CLONE_NEWUSER | 
-			syscall.CLONE_NEWPID | syscall.CLONE_NEWNS | syscall.CLONE_NEWUTS,
+		Cloneflags: cloneFlags,
 	}
 
-	cmd.Stdin = os.Stdin
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-
-	fmt.Println("RUN PID =", os.Getpid())
-	cwd, err := os.Getwd()
-	if err != nil {
-		panic(err)
+	if os.Getuid() != 0 {
+		cmd.SysProcAttr.Cloneflags |= syscall.CLONE_NEWUSER
+		cmd.SysProcAttr.UidMappings = []syscall.SysProcIDMap{
+			{ContainerID: 0, HostID: os.Getuid(), Size: 1},
+		}
+		cmd.SysProcAttr.GidMappings = []syscall.SysProcIDMap{
+			{ContainerID: 0, HostID: os.Getgid(), Size: 1},
+		}
 	}
-	cmd.Dir = cwd
 
-	cmd.Run()
-	
-	// cmd.Start()
-	// pid := cmd.Process.Pid
-	// fmt.Println("Child PID:", pid)
-	// fmt.Println("Host UID:", os.Getuid(), "Host GID:", os.Getgid())
+	if term.IsTerminal(int(os.Stdin.Fd())) {
+		f, err := pty.Start(cmd)
+		if err != nil {
+			panic(fmt.Sprintf("failed to start with PTY: %v", err))
+		}
+		defer f.Close()
 
-	// uidMap := fmt.Sprintf("0 %d 1\n", os.Getuid())
-	// err := os.WriteFile(fmt.Sprintf("/proc/%d/uid_map", pid), []byte(uidMap), 0)
-	// if err != nil {
-	// 	panic(err)
-	// }
+		ch := make(chan os.Signal, 1)
+		signal.Notify(ch, syscall.SIGWINCH)
+		go func() {
+			for range ch {
+				if err := pty.InheritSize(os.Stdin, f); err != nil {
+					fmt.Printf("error resizing pty: %s\n", err)
+				}
+			}
+		}()
+		ch <- syscall.SIGWINCH
+		defer func() { signal.Stop(ch); close(ch) }()
 
-	// err = os.WriteFile(fmt.Sprintf("/proc/%d/setgroups", pid), []byte("deny"), 0)
-	// if err != nil {
-	// 	panic(err)
-	// }
+		oldState, err := term.MakeRaw(int(os.Stdin.Fd()))
+		if err != nil {
+			panic(err)
+		}
+		defer term.Restore(int(os.Stdin.Fd()), oldState)
 
-	// gidMap := fmt.Sprintf("0 %d 1\n", os.Getgid())
-	// err = os.WriteFile(fmt.Sprintf("/proc/%d/gid_map", pid), []byte(gidMap), 0)
-	// if err != nil {
-	// 	panic(err)
-	// }
+		go func() { io.Copy(f, os.Stdin) }()
+		io.Copy(os.Stdout, f)
 
-	// cmd.Wait()
+		cmd.Wait()
+	} else {
+		cmd.Stdin = os.Stdin
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		if err := cmd.Start(); err != nil {
+			panic(err)
+		}
+		cmd.Wait()
+	}
 }
 
 func child() {
-	// cmd := os.Args[2]
 	args := os.Args[2:]
-	fmt.Println("Running inside child")
 
-	syscall.Sethostname([]byte("container"))
-	syscall.Mount("", "/", "", syscall.MS_PRIVATE|syscall.MS_REC, "")
-	// syscall.Mount("proc", "/proc", "proc", 0, "")
+	// Identify the host PTY slave path before we pivot
+	// This helps the 'tty' command inside the container.
+	hostPtyPath, _ := os.Readlink("/proc/self/fd/0")
 
-	if !strings.HasPrefix(args[0], "/") {
-		fmt.Println("Error: command must be an absolute path")
+	must(syscall.Mount("", "/", "", syscall.MS_PRIVATE|syscall.MS_REC, ""))
+
+	rootfs := os.Getenv("ROOTFS_PATH")
+	if rootfs == "" {
+		panic("ROOTFS_PATH not set")
+	}
+	absRootfs, err := filepath.Abs(rootfs)
+	must(err)
+	rootfs = absRootfs
+
+	must(syscall.Mount(rootfs, rootfs, "", syscall.MS_BIND|syscall.MS_REC, ""))
+
+	oldroot := filepath.Join(rootfs, "oldroot")
+	must(os.MkdirAll(oldroot, 0700))
+
+	must(syscall.PivotRoot(rootfs, oldroot))
+	must(os.Chdir("/"))
+
+	// Mount Pseudo-filesystems
+	must(syscall.Mount("proc", "/proc", "proc", 0, ""))
+	must(syscall.Mount("sysfs", "/sys", "sysfs", syscall.MS_RDONLY, ""))
+	must(syscall.Mount("tmpfs", "/dev", "tmpfs", syscall.MS_NOSUID|syscall.MS_NOEXEC, "mode=755"))
+
+	// Setup Device Nodes
+	// If we have a TTY on the host, bind mount it to /dev/console
+	if hostPtyPath != "" && strings.HasPrefix(hostPtyPath, "/dev/pts/") {
+		// We must create the mount point first
+		f, _ := os.Create("/dev/console")
+		if f != nil {
+			f.Close()
+			// Use the oldroot path to access the host's PTY device
+			fullHostPtyPath := filepath.Join("/oldroot", hostPtyPath)
+			if err := syscall.Mount(fullHostPtyPath, "/dev/console", "", syscall.MS_BIND, ""); err != nil {
+				// Fallback to mknod if bind mount fails
+				must(syscall.Mknod("/dev/console", syscall.S_IFCHR|0600, int(unix.Mkdev(5, 1))))
+			}
+		}
+	} else {
+		must(syscall.Mknod("/dev/console", syscall.S_IFCHR|0600, int(unix.Mkdev(5, 1))))
+	}
+
+	must(syscall.Mknod("/dev/null", syscall.S_IFCHR|0666, int(unix.Mkdev(1, 3))))
+	must(syscall.Mknod("/dev/zero", syscall.S_IFCHR|0666, int(unix.Mkdev(1, 5))))
+	must(syscall.Mknod("/dev/full", syscall.S_IFCHR|0666, int(unix.Mkdev(1, 7))))
+	must(syscall.Mknod("/dev/random", syscall.S_IFCHR|0666, int(unix.Mkdev(1, 8))))
+	must(syscall.Mknod("/dev/urandom", syscall.S_IFCHR|0666, int(unix.Mkdev(1, 9))))
+
+	// Link /dev/tty to /dev/console
+	must(os.Symlink("/dev/console", "/dev/tty"))
+
+	must(os.MkdirAll("/dev/pts", 0755))
+	must(syscall.Mount("devpts", "/dev/pts", "devpts", 0, "newinstance,ptmxmode=0666,mode=0620"))
+	must(os.Symlink("/dev/pts/ptmx", "/dev/ptmx"))
+
+	must(os.Symlink("/proc/self/fd", "/dev/fd"))
+	must(os.Symlink("/proc/self/fd/0", "/dev/stdin"))
+	must(os.Symlink("/proc/self/fd/1", "/dev/stdout"))
+	must(os.Symlink("/proc/self/fd/2", "/dev/stderr"))
+
+	// 5. Setup Controlling Terminal
+	var origPgrp int
+	isTTY := term.IsTerminal(0)
+	if isTTY {
+		if pgrp, err := unix.IoctlGetInt(0, unix.TIOCGPGRP); err == nil {
+			origPgrp = pgrp
+		}
+		// TIOCSCTTY: set controlling terminal
+		if err := unix.IoctlSetInt(0, unix.TIOCSCTTY, 1); err != nil {
+			fmt.Fprintf(os.Stderr, "Warning: failed to set TIOCSCTTY: %v\n", err)
+		}
+	}
+
+	must(syscall.Unmount("/oldroot", syscall.MNT_DETACH))
+	must(os.RemoveAll("/oldroot"))
+
+	must(syscall.Sethostname([]byte("container")))
+
+	cmdPath, err := exec.LookPath(args[0])
+	if err != nil {
+		fmt.Printf("Command not found: %s\n", args[0])
 		os.Exit(1)
 	}
 
-	ttyFD := int(os.Stdin.Fd())
-
-	origPgrp, err := unix.IoctlGetInt(ttyFD, unix.TIOCGPGRP)
-	if err != nil {
-		panic(err)
-	}
-
-	newRoot := "./rootfs"
-	oldRoot := "./rootfs/oldroot"
-
-	// if err := os.Chdir(newRoot); err != nil {
-	// 	panic(err)
-	// }
-	
-	if err := syscall.Mount(newRoot, newRoot, "", syscall.MS_BIND|syscall.MS_REC, ""); err != nil {
-		panic("bind mount failed: " + err.Error())
-	}
-
-	if err := os.MkdirAll(oldRoot, 0700); err != nil {
-		panic(err)
-	}
-	
-	// pivot using relative paths
-	if err := syscall.PivotRoot(newRoot, oldRoot); err != nil {
-		panic("pivot_root failed: " + err.Error())
-	}
-
-	if err := os.Chdir("/"); err != nil {
-		panic(err)
-	}
-
-	// syscall.Mount("proc", "/proc", "proc", 0, "")
-	// syscall.Mount("sysfs", "/sys", "sysfs", 0, "")
-	// syscall.Mount("tmpfs", "/dev", "tmpfs", 0, "")
-
-	if err := syscall.Unmount("/oldroot", syscall.MNT_DETACH); err != nil {
-		panic(err)
-	}
-	os.RemoveAll("/oldroot")
-
-	// syscall.Exec(cmd, args, os.Environ())
 	pid, err := syscall.ForkExec(
-		args[0],
+		cmdPath,
 		args,
 		&syscall.ProcAttr{
-			Files: []uintptr{
-				os.Stdin.Fd(),
-				os.Stdout.Fd(),
-				os.Stderr.Fd(),
-			},
+			Env:   os.Environ(),
+			Files: []uintptr{0, 1, 2},
 		},
 	)
+	must(err)
 
-	if err != nil {
-		panic(err)
-	}
-
-	syscall.Setsid()
 	syscall.Setpgid(pid, pid)
-	unix.IoctlSetInt(ttyFD, unix.TIOCSPGRP, pid)
+	if isTTY {
+		_ = unix.IoctlSetInt(0, unix.TIOCSPGRP, pid)
+	}
 
 	sigCh := make(chan os.Signal, 1)
 	signal.Notify(sigCh, syscall.SIGTERM, syscall.SIGINT, syscall.SIGQUIT)
 
-	fmt.Println("Init PID:", os.Getpid(), "Child PID:", pid)
-	
 	go func() {
 		for sig := range sigCh {
-			syscall.Kill(pid, sig.(syscall.Signal))
+			_ = syscall.Kill(pid, sig.(syscall.Signal))
 		}
 	}()
-		
-	var exitCode int
 
+	var exitCode int
 	for {
 		var ws syscall.WaitStatus
 		wpid, err := syscall.Wait4(-1, &ws, 0, nil)
 		if err != nil {
-			break
+			if err == syscall.ECHILD {
+				break
+			}
+			continue
 		}
-
 		if wpid == pid {
 			if ws.Exited() {
 				exitCode = ws.ExitStatus()
 			} else if ws.Signaled() {
 				exitCode = 128 + int(ws.Signal())
 			}
-			break
 		}
 	}
 
-	unix.IoctlSetInt(ttyFD, unix.TIOCSPGRP, origPgrp)
-	os.Exit(exitCode)
+	if isTTY && origPgrp != 0 {
+		_ = unix.IoctlSetInt(0, unix.TIOCSPGRP, origPgrp)
+	}
 
+	os.Exit(exitCode)
+}
+
+func must(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
